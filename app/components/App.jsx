8440126cefcf437c4d93933fe9c07ffe49cb3219
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-11-05 20:30:37 +0545

    Tracking Note editing State

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -26,7 +26,12 @@ export default class App extends Component {
     return (
       <div>
         <button onClick={() => this.addNote()}>+</button>
-        <Notes notes={notes} onDelete={this.deleteNote}/>
+        <Notes
+          notes={notes}
+          onNoteClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote}
+        />
       </div>
     );
   }
@@ -50,6 +55,31 @@ export default class App extends Component {
     this.setState({
       ...this.state,
       notes: this.state.notes.filter(note => note.id !== id)
+    });
+  }
+
+  activateNoteEdit = (id) => {
+    this.setState({
+      ...this.state,
+      notes: this.state.notes.map(note => {
+        if (note.id === id) {
+          note.editing = true;
+        }
+        return note;
+      })
+    });
+  }
+
+  editNnote = (id, task) => {
+    this.setState({
+      ...this.state,
+      notes: this.state.notes.map(note => {
+        if (note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
+        return note;
+      })
     });
   }
 }
